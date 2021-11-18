43c230e3320be5c4f6f305e821087f52f62b303d
Justine <hell.justine@gmail.com>
2021-11-18 18:08:21 +0100

    Building Pagination

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,6 +2,23 @@
   <div class="events">
     <h1>Event For Good</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        >&#60; Previous</router-link
+      >
+      <router-link
+        id="page-next"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+        >Next &#62;</router-link
+      >
+    </div>
   </div>
 </template>
 
@@ -9,25 +26,45 @@
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue';
 import EventService from '@/services/EventService.js';
+import { watchEffect } from 'vue';
 
 export default {
   name: 'EventList',
+  props: {
+    page: {
+      type: Number,
+      default: 1,
+    },
+  },
   components: {
     EventCard,
   },
   data() {
     return {
       events: null,
+      totalEvents: 0,
     };
   },
   created() {
-    EventService.getEvents()
-      .then((response) => {
-        this.events = response.data;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    // When reactives objects that are accessed inside this function change (eg. page), run this function again
+    watchEffect(() => {
+      // Clear out the events on the page, so our user knows the API has been called
+      this.events = null;
+      EventService.getEvents(2, this.page)
+        .then((response) => {
+          this.events = response.data;
+          this.totalEvents = response.headers['x-total-count'];
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    });
+  },
+  computed: {
+    hasNextPage() {
+      let totalPages = Math.ceil(this.totalEvents / 2);
+      return this.page < totalPages;
+    },
   },
 };
 </script>
@@ -36,5 +73,20 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+#page-prev {
+  text-align: left;
+}
+#page-next {
+  text-align: right;
 }
 </style>
