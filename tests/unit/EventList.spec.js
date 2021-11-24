6e075032e23d4babbf0c3ce0734cface9fc27fa7
Justine <hell.justine@gmail.com>
2021-11-24 12:07:10 +0100

    Mocking Vuex Stores - Part 2 Testing EventList Component - Show list of event with the necessary information

--- tests/unit/EventList.spec.js
+++ tests/unit/EventList.spec.js
@@ -1,28 +1,58 @@
 import { mount } from '@vue/test-utils'
 import EventList from '@/views/EventList'
-import store from '@/store'
+import { createStore } from '@/store'
 import router from '@/router'
+import { events as mockEvents } from '../../db.json'
+
+function mountEventList(config = {}) {
+  config.mountOptions = config.mountOptions || {}
+  config.plugins = config.plugins || {}
+  return mount(EventList, {
+    global: {
+      plugins: [createStore(config.plugins.store), router],
+    },
+    ...config.mountOptions,
+  })
+}
+
+let wrapper
 
 describe('EventList', () => {
+  beforeEach(() => {
+    wrapper = mountEventList()
+  })
+
   it('should render the events', () => {
-    const wrapper = mount(EventList, {
-      global: {
-        plugins: [store, router],
-      },
-    })
     expect(wrapper.exists()).toBeTruthy()
   })
 
   describe('page title', () => {
     it('is rendered with the correct text', () => {
-      const wrapper = mount(EventList, {
-        global: {
-          plugins: [store, router],
-        },
-      })
       const title = wrapper.find('[data-testid="event-list-title"]')
       expect(title.exists()).toBeTruthy()
       expect(title.text()).toContain('Events for Good')
+    })
+  })
+
+  describe('events', () => {
+    it('are rendered in a list with the necessary information', () => {
+      wrapper = mountEventList({
+        plugins: {
+          store: {
+            state: () => ({
+              events: mockEvents,
+            }),
+          },
+        },
+      })
+      const events = wrapper.findAll('[data-testid="event"]')
+      expect(events).toHaveLength(mockEvents.length)
+
+      events.forEach((event, i) => {
+        const eventText = event.text()
+        expect(eventText).toContain(mockEvents[i].title)
+        expect(eventText).toContain(mockEvents[i].date)
+      })
     })
   })
 })
