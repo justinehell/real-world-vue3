5baad29622c4fb75ae7537b632682e5a9c24d217
Justine <hell.justine@gmail.com>
2021-11-30 14:06:26 +0100

    BONUS - Todo Component

---
+++ src/views/Todo.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="todo-page">
+    <h1>Todo</h1>
+    <form @submit.prevent>
+      <div class="mb1">
+        <label for="task-label">Task Label: </label>
+        <input type="text" id="task-label" v-model="newTask.label" />
+      </div>
+      <div class="mb1">
+        <label for="task-type">Take Type: </label>
+        <select name="task-type" id="task-type" v-model="newTask.type">
+          <option value="personal">Personal</option>
+          <option value="work">Work</option>
+          <option value="miscellaneous">Miscellaneous</option>
+        </select>
+      </div>
+      <button @click="addTask">Add Task</button>
+    </form>
+    <h2>Task List</h2>
+    <div>
+      <button @click="listFilter = 'all'">All</button> |
+      <button @click="listFilter = 'incomplete'">Incomplete</button> |
+      <button @click="listFilter = 'complete'">Complete</button>
+    </div>
+    <ul>
+      <li v-for="(task, index) in filteredTasks" :key="`task-${index}`">
+        <input type="checkbox" v-model="task.isComplete" /> {{ task.label }} ({{
+          task.type
+        }})
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { TodoItem } from '../types'
+export default defineComponent({
+  data: () => ({
+    newTask: {
+      label: '',
+      type: 'personal',
+    } as TodoItem,
+    taskItems: [] as TodoItem[],
+    listFilter: 'all',
+  }),
+  computed: {
+    filteredTasks(): TodoItem[] {
+      if (this.listFilter === 'complete') {
+        return this.taskItems.filter(
+          (item: TodoItem) => item.isComplete === true
+        )
+      } else if (this.listFilter === 'incomplete') {
+        return this.taskItems.filter(
+          (item: TodoItem) => item.isComplete === false
+        )
+      } else {
+        return this.taskItems
+      }
+    },
+  },
+  methods: {
+    addTask() {
+      this.taskItems.push({
+        ...this.newTask,
+        isComplete: false,
+      })
+    },
+  },
+})
+</script>
+
+<style scoped>
+.todo-page {
+  max-width: 800px;
+  margin: 0 auto;
+}
+ul {
+  list-style: none;
+}
+.mb1 {
+  margin-bottom: 1rem;
+}
+</style>
