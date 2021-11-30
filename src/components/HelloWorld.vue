71e20c2a95bbbf1d1416c5b313d3ce9389350c12
Justine <hell.justine@gmail.com>
2021-11-30 11:25:35 +0100

    Setting Up Vue3 & TypeScript vue add typescript command

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -78,13 +78,15 @@
   </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
   name: 'HelloWorld',
   props: {
     msg: String,
   },
-}
+})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,12 +5,14 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent } from 'vue'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
-export default {
+
+export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld,
   },
-}
+})
 </script>
