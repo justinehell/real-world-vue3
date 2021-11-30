9a968fda7fc24c75bc24b399ac480081441962a2
Justine <hell.justine@gmail.com>
2021-11-30 11:17:41 +0100

    - Intro to Vue3 + TypeScript - Project Setup

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,26 +1,19 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const apiClient = axios.create({
-  // baseURL: 'https://my-json-server.typicode.com/Code-Pop/Touring-Vue-Router',
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
   },
-});
+})
 
 export default {
-  // getEvents(perPage, page) {
-  //   return apiClient.get(`/events?_limit=${perPage}&_page=${page}`);
-  // },
   getEvents() {
-    return apiClient.get(`/events`);
+    return apiClient.get('/events')
   },
   getEvent(id) {
-    return apiClient.get(`/events/${id}`);
+    return apiClient.get('/events/' + id)
   },
-  postEvent(event) {
-    return apiClient.post(`/events`, event);
-  },
-};
+}
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+}
+</script>
